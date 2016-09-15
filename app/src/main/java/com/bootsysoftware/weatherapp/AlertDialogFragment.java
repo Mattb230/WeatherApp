2af925157b9588c3dd509e60db39bbb8c49b0ec1
mattb230 <mr0tsplease>
2016-09-15 14:14:06 -0500

    Added fragment to tell user if the http connection failed. Added check for network status in main activity onCreate to

---
+++ app/src/main/java/com/bootsysoftware/weatherapp/AlertDialogFragment.java
@@ -0,0 +1,23 @@
+package com.bootsysoftware.weatherapp;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by Matthew Boydston on 9/15/2016.
+ */
+public class AlertDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context).setTitle(R.string.error_titleString)
+                .setMessage(R.string.error_messageString)
+                .setPositiveButton(R.string.okString, null);
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+}
--- app/src/main/java/com/bootsysoftware/weatherapp/MainActivity.java
+++ app/src/main/java/com/bootsysoftware/weatherapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.bootsysoftware.weatherapp;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -21,32 +25,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //create the URL and store it in forecastUrL
         String apiKey = "c62c5864bec90aa70b94605fb5490a04";
         double lat =37.8267;
         double lon =-122.423;
         String forecastURL = "https://api.forecast.io/forecast/" + apiKey + "/" + lat + "," + lon;
-        //String forecastURL = "https://api.forecast.io/forecast/c62c5864bec90aa70b94605fb5490a04/37.8267,-122.423";
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(forecastURL).build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                try {
-                    if(response.isSuccessful()) {
-                        Log.v(TAG, response.body().string());
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG, "Exception caught: ", e);
+        if(isNetworkAvailable()) {
+
+            //Create the okHttpClient and build the request
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(forecastURL).build();
+            //make the HTTP call
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
                 }
-            }
-        });//end call.enqueue
 
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
 
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                }
+            });//end call.enqueue
+        } else {
+            Toast.makeText(MainActivity.this, R.string.network_unavaileString, Toast.LENGTH_SHORT).show();
+        }
     }//end onCreate
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if(networkInfo != null && networkInfo.isConnected()){
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
+    }
 }//end class
 
